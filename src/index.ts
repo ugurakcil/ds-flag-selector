c33cdb7638ed19c39d82a9787028c70c5d4eb9a9
ugurakcil <ugurakcil@gmail.com>
2022-12-13 14:47:29 +0300

    ds init

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,56 @@
-import { subtract } from "./libraries/app";
-import './index.scss'
+// import { subtract } from "./libraries/app";
+import './styles/index.scss'
 
-function init() {
-    const form = document.querySelector("form");
-    form?.addEventListener("submit", submitHandler);
-  }
-
-function submitHandler(e: Event) {
-  e.preventDefault();
-  const num1 = document.querySelector("input[name='a']") as HTMLInputElement;
-  const num2 = document.querySelector("input[name='b']") as HTMLInputElement;
-  const result = subtract(Number(num1.value), Number(num2.value));
-  const resultElement = document.querySelector("#display");
-  if (resultElement) {
-    resultElement.textContent = result.toString();
-  }
+interface Options {
+  targetInput?: string;
+  /*
+  answer?: number,
+  aMethod?: (a:string) => string;
+  */
 }
 
-init();
+export default class dsFlagSelector {
+  options: Options;
+
+  constructor(options: Options) {
+    this.options = {
+      targetInput: 'bar',
+      /*
+      answer: 42,
+      aMethod: function(){}
+      */
+    };
+
+    Object.assign(this.options, options);
+
+    //document.addEventListener('DOMContentLoaded', this.doSomething);
+    console.info('değis bi')
+
+    this.doSomething()
+  }
+
+  doSomething() {
+    console.warn('Heheheheh')
+  }
+  
+}
+
+
+/*
+
+  constructor() {
+    const form = document.querySelector("form");
+    form?.addEventListener("submit", this.submitHandler);
+  }
+
+  submitHandler(e: Event) {
+    e.preventDefault();
+    const num1 = document.querySelector("input[name='a']") as HTMLInputElement;
+    const num2 = document.querySelector("input[name='b']") as HTMLInputElement;
+    const result = subtract(Number(num1.value), Number(num2.value));
+    const resultElement = document.querySelector("#display");
+    if (resultElement) {
+      resultElement.textContent = result.toString();
+    }
+  }
+*/
